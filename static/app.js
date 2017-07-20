a07ae2b51e5f4955a32fcf9e6ea9ea387f8527ef
Vasilis Nicolaou <vaslabs@yahoo.gr>
2017-07-20 21:50:42 +0100

    Root auto-focus

--- static/app.js
+++ static/app.js
@@ -14,6 +14,20 @@ app.directive('ngEnter', function() {
         };
 });
 
+app.directive('focusMe', function() {
+  return {
+    link: function(scope, element, attrs) {
+      scope.$watch(attrs.focusMe, function(value) {
+        if(value === true) {
+          console.log('value=',value);
+            element[0].focus();
+            scope[attrs.focusMe] = false;
+        }
+      });
+    }
+  };
+});
+
 app.controller('MainController', function($q, $http) {
 
     var ctrl = this;
